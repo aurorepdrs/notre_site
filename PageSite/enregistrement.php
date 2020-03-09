f873f9c880b05f793b79c2c66e5327e8ab36c478
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-09 15:19:00 +0100

    modif enregistrement

--- PageSite/enregistrement.php
+++ PageSite/enregistrement.php
@@ -4,20 +4,28 @@
     <head>
         
     </head>
-    <body>
-  <h1>toto</h1>  
+    <body> 
 <?php
-        
+function enregistrer($nom,$prenom,$tel,$mail,$mdp1,$genre,$voie,$adr,$cadr,$cp){     
+        $sql = "INSERT  INTO UTILISATEUR(nom,prénom,tel,email,motdepasse,sexe,num_voie,nom_voie,complement_adr,code_postal) VALUES('".$nom."','".$prenom."',".$tel.",'".$mail."','".$mdp1."','".$genre."',".$voie.",'".$adr."','".$cadr."',".$cp.")";
+    
+        echo "req ".$sql."<br>";
+                
          try
-{
-  $bdd = new PDO('mysql:host=localhost;dbname=ProjetS6;', 'root', 'root');
- $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+        {
+          $bdd = new PDO('mysql:host=localhost;dbname=ProjetS6;', 'root', 'root');
+         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                               $bdd->query($sql);
 
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
+    }
+
+      
+        $ok=true;
         
         $nom = $_GET["nom"];
         echo "nom ".$nom."<br>";
@@ -29,8 +37,6 @@ catch(Exception $e)
         echo "mail ".$mail."<br>";
         $mdp1 = $_GET["mdp1"];
         echo "mdp1 ".$mdp1."<br>";
-        $mdp2 = $_GET["mdp2"];
-        echo "mdp2 ".$mdp2."<br>";
         $genre = $_GET["genre"];
         echo "genre ".$genre."<br>";
         $voie = $_GET["voie"];
@@ -42,32 +48,72 @@ catch(Exception $e)
         $cp = $_GET["cp"];
         echo "cp ".$cp."<br>";
         
-        echo "nom avant requete ".$nom."<br>";
-
-        $sql = "INSERT  INTO UTILISATEUR(nom,prénom,tel,email,motdepasse,motdepasse2,sexe,num_voie,nom_voie,complement_adr,code_postal) VALUES('$nom','$prenom',$tel,'$mail','$mdp1','$mdp2','$genre',$voie,'$adr','$cadr',$cp)";
-        
-        
-        
-        echo "sql ".$sql."<br>";
-
-$rep = $bdd->query('select nom from UTILISATEUR');
-  
-        
-
-        while ($ligne = $rep ->fetch())  { 
-          echo "<tr>";
-           echo "<td>".$ligne['nom']."<br />\n"."</td>";
-            echo"</tr>\n";
-        
+         if($_GET['nom']==""){
+echo "Le champ nom n’a pas été saisi </br>";
+$ok=false;   
+        }
+         if($_GET['prenom']==""){
+echo "Le champ prenom n’a pas été saisi </br>";
+$ok=false;
 }
-$rep ->closeCursor();
+        if($_GET['tel']==""){
+echo "Le champ téléphone n’a pas été saisi</br>";
+$ok=false;
+}
+        if($_GET['mail']==""){
+echo "Le champ email n’a pas été saisi</br>";
+$ok=false;
+}
+        
+        if($_GET['mdp1']!= $_GET['mdp2'] or $_GET['mdp1']=="" or $_GET['mdp2']==""){
+echo "Les mots de passe ne sont pas similaires ou n'ont pas été saisi";
+$ok=false;
+        }
+        if($_GET['genre']==""){
+echo "Le champ genre n’a pas été saisi</br>";
+$ok=false;
+}
+            if($_GET['voie']==""){
+echo "Le champ n° de voie n’a pas été saisi</br>";
+$ok=false;
+} 
+        if($_GET['adr']==""){
+echo "Le champ adresse n’a pas été saisi</br>";
+$ok=false;
+}
+        if($_GET['cadr']==""){
+echo "Le champ complément d'adresse n’a pas été saisi</br>";
+$ok=false;
+}
+        if($_GET['cp']==""){
+echo "Le champ code postal n’a pas été saisi</br>";
+$ok=false;
+}
+
+    if ($ok==false ){
+        
+        echo"<meta http-equiv='refresh'content='0;URL=PageInscription.html'>";
+    }      
+            
+
+      else{
+       
+       enregistrer($nom,$prenom,$tel,$mail,$mdp1,$genre,$voie,$adr,$cadr,$cp);
+    echo "<meta http-equiv='refresh'content='0;URL=PageAccueil.html'>";
+      }
+
+        
+
+        
+
+
 
         
 
         ?>
         
         
-          <h1>titi</h1>  
+          
 
     </body>
 </html>
