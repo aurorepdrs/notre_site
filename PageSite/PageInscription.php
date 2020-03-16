d234dc81f7e5651bc27906e15fc9e5c1b177deef
aurorepdrs <aurorepdrs34@gmail.com>
2020-03-16 15:50:22 +0100

    modification de la page inscription

--- PageSite/PageInscription.php
+++ PageSite/PageInscription.php
@@ -31,7 +31,7 @@
 
 	<p> Nom : <input type="text" name="n" value=""/> </p>
 	<p> Prénom : <input type="text" name="p" value=""/> </p>
-	<p> Numéro de téléphone : <input type="number" name="num" value=""/> </p>
+	<p> Numéro de téléphone : <input type="text" name="num" value=""/> </p>
 	<p> Email : <input type="text" name="mail" value=""/> </p>
 	<p> Mot de passe : <input type="password" name="mdp1" value=""/> </p>
 	<p> Confirmation du mot de passe : <input type="password" name="mdp2" value=""/> </p>
@@ -48,7 +48,7 @@
 	<p> Numéro de voie : <input type="number" name="voie" value=""/> </p>
 	<p> Nom de la rue: <input type="text" name="adr" value=""/> </p>
 	<p> Complément d'adresse : <input type="text" name="cadr" value=""/> </p>
-	<p> Code postal : <input type="number" name="cp" value=""/> </p>
+	<p> Code postal : <input type="text" name="cp" value=""/> </p>
 
 	</td>
 	</tr>
