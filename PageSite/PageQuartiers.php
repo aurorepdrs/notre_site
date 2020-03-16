7d788078c2ac474c6e876d8e99158151d6ed8571
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-16 15:42:05 +0100

    modif page quartiers

---
+++ PageSite/PageQuartiers.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<link rel="stylesheet" href="Commerces.css" type="text/css"
+media="screen" />
+	<title>Commerces page php</title>
+<?php
+?>
+</head>
+<body>
+		
+	<div class="recherche_p">
+
+		<form action="/search" id="searchthis" method="get">
+			<input id="search" name="q" type="text" placeholder="Rechercher" />
+			<input id="search-btn" type="submit" value="Rechercher" />
+		</form>
+
+	</div>
+
+	</body>
+	</html>
+			
