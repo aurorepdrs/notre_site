10efefd7e5574f2f539fd8ff19251816cd4a18fe
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-23 16:59:06 +0100

    modif

---
+++ PageSite/PagePhp/MotDePasse.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="UTF-8">
+   <title>Mot de passe oublié</title>
+	<link rel="stylesheet" href="inscription.css" type="text/css" media="screen" />
+
+</head>
+
+<body>
+
+	<h2> <a href="PageAccueil.php"> Accueil</a> </h2>
+	
+	<div id="mdp"> 
+	<p> Tapez votre adresse mail, nous vous enverrons un mail pour réinitialiser votre mot de passe </p> 
+	<form method="get" action="connecte.php">
+       
+  	<p> Adresse mail : <br/>
+  	<input type="text" name="Adresse mail" placeholder="Ex: philippe@hotmail.com" /> <br/> </p>
+  	<p> <input type="submit" value="Envoyer"> </p> 
+  	</div>
+
+</body>
