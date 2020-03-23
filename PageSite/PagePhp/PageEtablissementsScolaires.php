39847cdd528ccb5bce7893eea48456093160a751
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-23 16:58:15 +0100

    modif

---
+++ PageSite/PagePhp/PageEtablissementsScolaires.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<link rel="stylesheet" href="Commerces.css" type="text/css"
+media="screen" />
+	<title>Etablissements Scolaires</title>
+<?php
+?>
+</head>
+<body>
+		<h2><a href="PageAccueil.php" >Accueil</a></h2>
+	<div class="recherche_p">
+
+		<form action="/search" id="searchthis" method="get">
+			<input id="search" name="q" type="text" placeholder="Rechercher" />
+			<input id="search-btn" type="submit" value="Rechercher" />
+		</form>
+
+	</div>
+
+	</body>
+	</html>
