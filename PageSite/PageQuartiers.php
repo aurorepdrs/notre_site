29508bea55ac8da7ebfbc9a6a3f5b9a54c25105e
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-23 14:33:09 +0100

    ajout cartes

--- PageSite/PageQuartiers.php
+++ PageSite/PageQuartiers.php
@@ -18,7 +18,20 @@ media="screen" />
 		</form>
 
 	</div>
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23100.830620636774!2d3.8481379999999996!3d43.635604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6aec119613691%3A0x13951bac733b922d!2sH%C3%B4pitaux-Facult%C3%A9s%2C%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584960487392!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
 
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11551.09174514465!2d3.8077782390720514!3d43.63208469675862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6ac25a8500dab%3A0xe6cedb4ee2427edb!2sLa%20Mosson%2C%2034080%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966806718!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    
+    <div> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11554.428300939058!2d3.8411167890645244!3d43.614722201254466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6aef42044b449%3A0x129f2d7be9ad785e!2sLes%20C%C3%A9vennes%2C%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966877076!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5777.590694005508!2d3.8731458772077256!3d43.61080258463078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af07764d6921%3A0xa44ea64f0246bb92!2sCtre%20Historique%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966924079!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23121.39475410537!2d3.844884916707907!3d43.582084751461956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6ae2d87555c79%3A0x6d04260d15b96a8e!2sLa%20Croix%20d&#39;Argent%2C%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584967073454!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11559.51697100562!2d3.875904639053051!3d43.58823155811146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6afc176a4a4dd%3A0x457ffea4717db5c6!2sPr%C3%A9s%20d&#39;Ar%C3%A8nes%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584967109922!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11556.866607199137!2d3.9009230000000006!3d43.60203045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af85d6a0e48d%3A0x6b4de2ce2f40baac!2sPort%20Marianne%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584970327018!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
 	</body>
 	</html>
 			
