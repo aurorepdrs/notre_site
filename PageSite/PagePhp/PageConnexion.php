6d16af79018db552cf8b3c5649f2099e7d1af7f8
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-23 16:57:45 +0100

    modif

---
+++ PageSite/PagePhp/PageConnexion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="UTF-8">
+    <title>Connexion</title>
+	<link rel="stylesheet" href="inscription.css" type="text/css" media="screen" />
+
+</head>
+  
+<body>
+
+	<h2> <a href="PageAccueil.php"> Accueil</a> </h2>
+
+	<div id="connexion">  
+  	<form method="get" action="connecte.php">
+       
+  	<p> Adresse mail : <br/>
+  	<input type="text" name="mail" placeholder="Ex: philippe@hotmail.com" /> <br/> </p>
+  	<p>Mot de passe : <br/>
+  	<input type="passeword" name="mdp1" placeholder="Ex: licorne34110" /> <br/> </p>
+	<p> <INPUT type="submit" value="Connexion"> </p>
+
+  	<p>Mot de passe oublié ? <br/>
+  	<a href= "MotDePasse.php">Cliquez ici</a> <br/> </p>
+          
+  	<a href= "PageInscription.php">S'inscrire</a><br/>
+          
+  	</form>
+  
+	</div>
+	
+</body>
+
+</html>
