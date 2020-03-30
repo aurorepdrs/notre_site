bef3702c42fa41ddf1f0f430bbd288587b7bcf96
lealeroy <47455548+lealeroy@users.noreply.github.com>
2020-03-30 15:18:09 +0200

    Delete Mosson.php

--- PageSite/PagePhp/Mosson.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    </head>
-    <body>
-        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11551.09174514465!2d3.8077782390720514!3d43.63208469675862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6ac25a8500dab%3A0xe6cedb4ee2427edb!2sLa%20Mosson%2C%2034080%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966806718!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-    </div>
-    </body>
-</html>
