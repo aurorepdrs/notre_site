24637156a2d019cd7bbac5c1dd6dfabac48ea580
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-30 15:06:17 +0200

    ajout pres d'arenes

---
+++ PageSite/PagePhp/PageQuartiers/PresDarenes.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    </head>
+    <body>
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11559.51697100562!2d3.875904639053051!3d43.58823155811146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6afc176a4a4dd%3A0x457ffea4717db5c6!2sPr%C3%A9s%20d&#39;Ar%C3%A8nes%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584967109922!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    </body>
+</html>
