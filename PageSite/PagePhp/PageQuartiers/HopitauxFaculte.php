3e365fe789f5d79daf2f6980a14647db610b01e3
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-30 15:07:43 +0200

    ajout hopitaux faculte

---
+++ PageSite/PagePhp/PageQuartiers/HopitauxFaculte.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    </head>
+    <body>
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23100.830620636774!2d3.8481379999999996!3d43.635604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6aec119613691%3A0x13951bac733b922d!2sH%C3%B4pitaux-Facult%C3%A9s%2C%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584960487392!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    </body>
+</html>
