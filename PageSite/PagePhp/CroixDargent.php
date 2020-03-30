161d6a23eb37f19a9066d0186f64ab4e1c3cf5b6
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-30 14:59:21 +0200

    ajout croix d'argent

---
+++ PageSite/PagePhp/CroixDargent.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    </head>
+    <body>
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23121.39475410537!2d3.844884916707907!3d43.582084751461956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6ae2d87555c79%3A0x6d04260d15b96a8e!2sLa%20Croix%20d&#39;Argent%2C%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584967073454!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    </body>
+</html>
