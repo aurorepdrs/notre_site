2565fe588687b197921d8f2ce689a454f4ce9ff8
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-30 15:08:38 +0200

    ajout centre

---
+++ PageSite/PagePhp/PageQuartiers/Centre.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    </head>
+    <body>
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5777.590694005508!2d3.8731458772077256!3d43.61080258463078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af07764d6921%3A0xa44ea64f0246bb92!2sCtre%20Historique%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966924079!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+    </body>
+</html>
