84159e0fc134875fc9a9a11d77da0b467d6d17b3
aurorepdrs <aurorepdrs34@gmail.com>
2020-03-30 15:50:26 +0200

    ajout de la page loisirs

--- PageSite/PagePhp/PageLoisirs.php
+++ PageSite/PagePhp/PageLoisirs.php
@@ -1,24 +1,42 @@
 <!DOCTYPE HTML>
 
 <html>
+
+
 <head>
-	<link rel="stylesheet" href="Commerces.css" type="text/css"
-media="screen" />
+	<link rel="stylesheet" href=".css" type="text/css" media="screen" />
 	<title>Loisirs</title>
-<?php
-?>
 </head>
+
 <body>
-		<h2><a href="PageAccueil.php" >Accueil</a></h2>
-	<div class="recherche_p">
+	<h1> Loisirs </h1>
+	<form action="enregistrement_loisirs.php" method="get" autocomplete="off"> 
+	<p>Piscine <input type="checkbox" name="piscine" value=""> </p>
+	<p>Plateaux Sportifs <input type="checkbox" name="plateaux_sportifs" value=""> </p>
+	<p>Terrains de jeux <input type="checkbox" name="terrains_jeux" value=""> </p>
+	<p>Salles de combat <input type="checkbox" name="salles_de_combat" value=""> </p>
+	<p>Skateparks <input type="checkbox" name="skateparks" value=""> </p>
+	<p>Bowlings <input type="checkbox" name="bowlings" value=""> </p>
+	<p>Salles de remise en forme <input type="checkbox" name="salles_de_ remise_en_forme" value=""> </p>
+	<p>Gymnases <input type="checkbox" name="gymnases" value=""> </p>
+	<p>Port <input type="checkbox" name="port" value=""> </p>
+	<p>Randonnées <input type="checkbox" name="randonnees" value=""> </p>
+	<p>Cinémas <input type="checkbox" name="cinemas" value=""> </p>
+	<p>Boulodromes <input type="checkbox" name="boulodromes" value=""> </p>
+	<p>Musées <input type="checkbox" name="musees" value=""> </p>
+	<p>Conservatoires <input type="checkbox" name="conversatoire" value=""> </p>
+	<p>Théâtres <input type="checkbox" name="theatres" value=""> </p>
+	<p>Terrains de tennis <input type="checkbox" name="terrains_de_tennis" value=""> </p>
+	<p>Cyclisme <input type="checkbox" name="cyclisme" value=""> </p>
+	<p>Domaines skiables <input type="checkbox" name="domaines_skiables" value=""> </p>
+	<p>Centres équestres <input type="checkbox" name="centres_equestres" value=""> </p>
+	<p>Athlétisme <input type="checkbox" name="athletisme" value=""> </p>
+	<p>Terrains de golf <input type="checkbox" name="terrains_de_golf" value=""> </p>
+	<p>Parcours sportifs <input type="checkbox" name="parcours_sportifs" value=""> </p>	
 
-		<form action="/search" id="searchthis" method="get">
-			<input id="search" name="q" type="text" placeholder="Rechercher" />
-			<input id="search-btn" type="submit" value="Rechercher" />
-		</form>
+	<p><a href="PageCommerces.php"> Suivant </a></p>
+</body>
 
-	</div>
 
-	</body>
-	</html>
+</html>
 	
