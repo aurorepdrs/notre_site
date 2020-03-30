da43714bcd553ed34db7ad5f3b2533f27d3bcf58
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-30 16:48:26 +0200

    modif

--- PageSite/PagePhp/PageQuartiers/Centre.php
+++ PageSite/PagePhp/PageQuartiers/Centre.php
@@ -1,8 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    </head>
+     <link rel="stylesheet" href="../../quartiers.css" type="text/css" />
+  <meta charset="UTF-8"/>
+    <title>Centre</title>
+  </head>
+  
+<header>
+            <h1>
+                <img id="logo" src="../../logo.png"></h1></header>
+  <nav>
+           
+            <ul class="menu">
+                <li ><a href="../PageAccueil.php" >Accueil</a></li>
+                <li ><a href="Cevennes.php" >Cevennes</a></li>
+                <li ><a href="Centre.php">Centre</a></li>
+                <li ><a href="CroixDargent.php">Croix d'argent</a></li>
+                <li ><a href="HopitauxFaculte.php">Hopitaux Faculté</a></li>
+                <li ><a href="Mosson.php"> Mosson</a></li>
+                <li ><a href="PortMarianne.php">Port Marianne</a></li>
+                <li ><a href="PresDarenes.php">Près d'arènes</a></li>
+                
+            </ul>
+
+        </nav>
     <body>
-        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5777.590694005508!2d3.8731458772077256!3d43.61080258463078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af07764d6921%3A0xa44ea64f0246bb92!2sCtre%20Historique%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966924079!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+        <h2> Quartier Centre</h2>
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5777.590694005508!2d3.8731458772077256!3d43.61080258463078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af07764d6921%3A0xa44ea64f0246bb92!2sCtre%20Historique%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966924079!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" center></iframe></div>
     </body>
 </html>
