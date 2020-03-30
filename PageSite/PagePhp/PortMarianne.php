94c58c8e87b0f2ffb8e0b82eb5bd789729890a1f
Léa  Leroy <lealeroy@MacBook-Pro-de-Lea.local>
2020-03-30 15:03:09 +0200

    mdf

--- PageSite/PagePhp/PortMarianne.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    </head>
-    <body>
-        <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11556.866607199137!2d3.9009230000000006!3d43.60203045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af85d6a0e48d%3A0x6b4de2ce2f40baac!2sPort%20Marianne%2C%2034000%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584970327018!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
-    </body>
-</html>
