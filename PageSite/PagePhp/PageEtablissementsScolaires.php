8fde68eb33ebe97197da1e9eddffa9c06ad5e255
aurorepdrs <aurorepdrs34@gmail.com>
2020-03-30 15:51:31 +0200

    ajout de la page etablissements

--- PageSite/PagePhp/PageEtablissementsScolaires.php
+++ PageSite/PagePhp/PageEtablissementsScolaires.php
@@ -1,23 +1,49 @@
 <!DOCTYPE HTML>
 
 <html>
+
+
 <head>
-	<link rel="stylesheet" href="Commerces.css" type="text/css"
-media="screen" />
-	<title>Etablissements Scolaires</title>
-<?php
-?>
+	<link rel="stylesheet" href=".css" type="text/css" media="screen" />
+	<title>Établissements scolaires</title>
 </head>
+
 <body>
-		<h2><a href="PageAccueil.php" >Accueil</a></h2>
-	<div class="recherche_p">
+	<h1> Établissements scolaires </h1>
+	<form action="enregistrement_etablissements.php" method="get" autocomplete="off"> 
+	<p>École maternelle <input type="checkbox" name="ecole_maternelle" value=""> </p>
+	<p>École maternelle avec cantine <input type="checkbox" name="ecole_maternelle_cantine" value=""> </p>
+	<p>École élémentaire <input type="checkbox" name="ecole_elementaire" value=""> </p>
+	<p>École élémentaire avec cantine <input type="checkbox" name="ecole_elementaire_cantine" value=""> </p>
+	<p>Collège <input type="checkbox" name="college" value=""> </p>
+	<p>Collège avec cantine <input type="checkbox" name="college_cantine" value=""> </p>
+	<p>Collège avec internat <input type="checkbox" name="college_internat" value=""> </p>
+	<p>Lycée général et technologique <input type="checkbox" name="lycee_general_technologique" value=""> </p>
+	<p>Lycée général et technologique avec cantine <input type="checkbox" name="lycee_general_technologique_cantine" value=""> </p>
+	<p>Lycée professionnel <input type="checkbox" name="lycee_professionnel" value=""> </p>
+	<p>Lycée professionnel avec cantine <input type="checkbox" name="lycee_professionnel_cantine" value=""> </p>
+	<p>Lycée professionnel avec internat <input type="checkbox" name="lycee_professionnel_internat" value=""> </p>
+	<p>Lycée technique <input type="checkbox" name="lycee_technique" value=""> </p>
+	<p>Lycée technique avec cantine <input type="checkbox" name="lycee_technique_cantine" value=""> </p>
+	<p>Lycée technique avec internat <input type="checkbox" name="lycee_technique_internat" value=""> </p>
+	<p>Prépa <input type="checkbox" name="prepa" value=""> </p>
+	<p>Formation santé <input type="checkbox" name="formation_sante" value=""> </p>
+	<p>Formation commerce <input type="checkbox" name="formation_commerce" value=""> </p>
+	<p>Formation non universitaire <input type="checkbox" name="formation_non_universitaire" value=""> </p>
+	<p>Formation recherche <input type="checkbox" name="formation_recherche" value=""> </p>
+	<p>Université <input type="checkbox" name="universite" value=""> </p>
+	<p>École ingénieur <input type="checkbox" name="ecole_ingenieur" value=""> </p>
+	<p>Enseignement supérieur privé <input type="checkbox" name="enseignement_superieur_prive" value=""> </p>
+	<p>Enseignement supérieur agricole <input type="checkbox" name="enseignement_superieur_agricole" value=""> </p>
+	<p>Enseignement supérieur agricole avec internat <input type="checkbox" name="enseignement_superieur_agricole_internat" value=""> </p>
+	<p>Autres enseignements supérieurs <input type="checkbox" name="autres_enseignements_superieurs" value=""> </p>
+	<p>Formation sport <input type="checkbox" name="formation_sport" value=""> </p>
+	<p>Résidence universitaire <input type="checkbox" name="residence_universitaire" value=""> </p>
+	<p>Restaurant universitaire <input type="checkbox" name="restaurant_universitaire" value=""> </p>
+	
+	<p><a href=""> Finaliser </a></p>
+</body>
 
-		<form action="/search" id="searchthis" method="get">
-			<input id="search" name="q" type="text" placeholder="Rechercher" />
-			<input id="search-btn" type="submit" value="Rechercher" />
-		</form>
 
-	</div>
-
-	</body>
-	</html>
+</html>
+	
