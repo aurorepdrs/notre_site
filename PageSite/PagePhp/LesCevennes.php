499ab3b5aa50196dd5255d4d70a28e04887e2298
lealeroy <47455548+lealeroy@users.noreply.github.com>
2020-03-30 15:18:02 +0200

    Delete LesCevennes.php

--- PageSite/PagePhp/LesCevennes.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    </head>
-    <body>
-        <div> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11554.428300939058!2d3.8411167890645244!3d43.614722201254466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6aef42044b449%3A0x129f2d7be9ad785e!2sLes%20C%C3%A9vennes%2C%20Montpellier!5e0!3m2!1sfr!2sfr!4v1584966877076!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
-    </body>
-</html>
