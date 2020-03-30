9b7676abbb9b4f8efcf284b8977b9903609f3896
aurorepdrs <aurorepdrs34@gmail.com>
2020-03-30 15:52:21 +0200

    ajout de la page commerces

--- PageSite/PagePhp/PageCommerces.php
+++ PageSite/PagePhp/PageCommerces.php
@@ -1,47 +1,45 @@
-<?php
-// on se connecte à notre base
- $bdd = new PDO('mysql:host=localhost;dbname=projetS6;', 'root', 'root');
+<!DOCTYPE HTML>
 
-?>
-		
 <html>
+
+
 <head>
-	<link rel="stylesheet" href="Commerces.css" type="text/css"
-media="screen" />
+	<link rel="stylesheet" href=".css" type="text/css" media="screen" />
 	<title>Commerces</title>
-
 </head>
+
 <body>
-<h2><a href="PageAccueil.php" >Accueil</a></h2>
-	<div class="recherche_p">
-
-		<form action="/search" id="searchthis" method="get">
-			<input id="search" name="q" type="text" placeholder="Rechercher" />
-			<input id="search-btn" type="submit" value="Rechercher" />
-		</form>
-	<div class="FormulaireCom">
-		<form>
-			
-		</form>
-	</div>
-	</div>	
-	<?php
+	<h1> Commerces </h1>
+	<form action="enregistrement_commerces.php" method="get" autocomplete="off"> 
+	<p>Hypermarché <input type="checkbox" name="hypermarche" value=""> </p>
+	<p>Librairie <input type="checkbox" name="librairie" value=""> </p>
+	<p>Magasins de vêtements <input type="checkbox" name="magasin_de_vetements" value=""> </p>
+	<p>Magasins du foyer <input type="checkbox" name="magasin_du_foyer" value=""> </p>
+	<p>Magasins de chaussures <input type="checkbox" name="magasin_de_chaussures" value=""> </p>
+	<p>Magasins d'électroménager <input type="checkbox" name="magasin_electromenager" value=""> </p>
+	<p>Magasins de meubles <input type="checkbox" name="magasin_de_meubles" value=""> </p>
+	<p>Magasins de sport <input type="checkbox" name="magasin_de_sport" value=""> </p>
+	<p>Magasins de mur <input type="checkbox" name="magasin_de_mur" value=""> </p>
+	<p>Droguerie <input type="checkbox" name="droguerie" value=""> </p>
+	<p>Parfumerie <input type="checkbox" name="parfumerie" value=""> </p>
+	<p>Supermarché <input type="checkbox" name="supermarche" value=""> </p>
+	<p>Bijouterie <input type="checkbox" name="bijouterie" value=""> </p>
+	<p>Fleuriste <input type="checkbox" name="fleuriste" value=""> </p>
+	<p>Magasins d'optique <input type="checkbox" name="magasins_optique" value=""> </p>
+	<p>Magasins du médical <input type="checkbox" name="magasins_du_medical" value=""> </p>
+	<p>Stations-service <input type="checkbox" name="stations_service" value=""> </p>
+	<p>Bricolage <input type="checkbox" name="bricolage" value=""> </p>
+	<p>Supérette <input type="checkbox" name="superette" value=""> </p>
+	<p>Épicerie <input type="checkbox" name="epicerie" value=""> </p>
+	<p>Boulangerie <input type="checkbox" name="boulangerie" value=""> </p>
+	<p>Boucherie <input type="checkbox" name="boucherie" value=""> </p>
+	<p>Magasins de surgelés <input type="checkbox" name="magasins_de_surgeles" value=""> </p>
+	<p>Poissonerie <input type="checkbox" name="poissonerie" value=""> </p>
 	
-		
-		$sql = 'SELECT Type_Commerce FROM COMMERCES';
-		$req = $bdd->query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-		// on va scanner tous les tuples un par un
-		while ($data = mysql_fetch_array($req)) {
-		// on affiche les résultats
-		echo 'type de commerce : '.$data['Type_Commerce'].'<br />';
-		
-}
-		mysql_free_result ($req);
-		mysql_close ();
-	?>
-	monprojet:
+
+	<p><a href="PageEtablissementsScolaires.php"> Suivant </a></p>
+</body>
 
 
-	</body>
-	</html>
+</html>
 	
