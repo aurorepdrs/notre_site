1df93d107c4164ff3a377e17a1f86f2e45797f91
aurorepdrs <aurorepdrs34@gmail.com>
2020-04-06 15:34:00 +0200

    modification

--- PageSite/PagePhp/PageConnexion.php
+++ PageSite/PagePhp/PageConnexion.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 
+
 <html>
 
 <head>
-
 	<meta charset="UTF-8">
     <title>Connexion</title>
-	<link rel="stylesheet" href="../inscription.css" type="text/css"  />
-
+	<link rel="stylesheet" href="../inscription.css" type="text/css" />
 </head>
   
 <body>
-
 	<h2> <a href="PageAccueil.php"> Accueil</a> </h2>
 
 	<div id="connexion">  
+	
   	<form method="get" action="connecte.php">
        
   	<p> Adresse mail : <br/>
   	<input type="text" name="mail" placeholder="Ex: philippe@hotmail.com" /> <br/> </p>
   	<p>Mot de passe : <br/>
-  	<input type="passeword" name="mdp1" placeholder="Ex: licorne34110" /> <br/> </p>
+  	<input type="passeword" name="mdp1" placeholder="Ex: licorne34" /> <br/> </p>
 	<p> <INPUT type="submit" value="Connexion"> </p>
 
   	<p>Mot de passe oublié ? <br/>
@@ -33,5 +32,6 @@
 	</div>
 	
 </body>
+
 
 </html>
