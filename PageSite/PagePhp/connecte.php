0ec5348a7af3ea524a6d5f7789fa8464101690ce
aurorepdrs <aurorepdrs34@gmail.com>
2020-04-06 15:32:59 +0200

    modification de la page pour se connecter

--- PageSite/PagePhp/connecte.php
+++ PageSite/PagePhp/connecte.php
@@ -1,64 +1,53 @@
 <!DOCTYPE html>
 
+
 <?php
 session_start();
 ?>
 
 <html>
 
-<head>
 
-<?php
+<head>
+	<?php
 		$bdd = new PDO('mysql:host=localhost:3306;dbname=projets6', 'root', 'root');
 		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql = "select email, motdepasse FROM utilisateur";
 		$statement = $bdd->prepare($sql);
 		$statement->execute();
-		
-		echo $sql."<br>"; 
-		
-		
-
-	$ok=TRUE;
-	if($_GET['mdp1']=="") {
-		echo "Le mot de passe n'a pas été saisi"."<br>";
-		$ok=FALSE;
-	}
-	if($_GET['mail']=="") {
-		echo "L'adresse mail n'a pas été saisie"."<br>";	
-		$ok=FALSE;
-	}
+				
+	
+		$ok=TRUE;
+		if($_GET['mdp1']=="") {
+			echo "Le mot de passe n'a pas été saisi"."<br>";
+			$ok=FALSE;
+		}
+		if($_GET['mail']=="") {
+			echo "L'adresse mail n'a pas été saisie"."<br>";	
+			$ok=FALSE;
+		}
 
 	
+		if(isset($_SESSION['utilisateur'])){
+			$estconnecte=TRUE;
+			$_SESSION['utilisateurconnecte']=$estconnecte;	
+		}
+		else{
+			$estconnecte=FALSE;
+			$_SESSION['utilisateurconnecte']=$estconnecte;
+		}
 
-	
-	if(isset($_SESSION['utilisateur'])){
-		$estconnecte=TRUE;
-		$_SESSION['utilisateurconnecte']=$estconnecte;
-		foreach($_SESSION['utilisateur'] as $table){
-		echo $table[0]." ",$table[1], '<br>';
-		}	
-	}
-	else{
-		$estconnecte=FALSE;
-		$_SESSION['utilisateurconnecte']=$estconnecte;
-	}
-	echo $_SESSION['utilisateurconnecte'];
-
-	if ($ok==FALSE) {
-		echo "<meta http-equiv='refresh'content='5;URL=PageConnexion.php'>";
-	}
-	else {
-		$_SESSION['utilisateur'] = $statement->fetchAll(PDO::FETCH_NUM);
-		echo "<meta http-equiv='refresh'content='5;URL=PageAccueil.php'>";
-	}
-	
-?>
-
+		if ($ok==FALSE) {
+			echo "<meta http-equiv='refresh'content='5;URL=PageConnexion.php'>";
+		}
+		else {
+			$_SESSION['utilisateur'] = $statement->fetchAll(PDO::FETCH_NUM);
+			echo "<meta http-equiv='refresh'content='0;URL=PageAccueil.php'>";
+		}
+	?>
 </head>
 
 <body>
-
 
 </body>
 
