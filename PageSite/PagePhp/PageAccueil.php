78b84962a287dd62e9b1a21a3d98c26592683aa5
KonateNindayou <60350204+KonateNindayou@users.noreply.github.com>
2020-04-10 14:50:39 +0200

    Add files via upload
    Rectification des erreurs du formulaire

--- PageSite/PagePhp/PageAccueil.php
+++ PageSite/PagePhp/PageAccueil.php
@@ -35,94 +35,96 @@ $estconnecte=$_SESSION['utilisateurconnecte'];
 
 	<h2>Trouvez le quartier de Montpellier qui correspondra parfaitement à vos attentes.</h2> 
 
-	<form action="recuperation.php" method="get" >
-	
-	<SELECT name="loisirs" >
-	<OPTION VALUE="">LOISIRS </OPTION>
-	<OPTION VALUE="Piscine">Piscine</OPTION>
-	<OPTION VALUE="Plateaux_sportifs">Plateaux Sportifs</OPTION> 
-	<OPTION VALUE="Terrains_jeux">Terrains de jeux</OPTION> 
-	<OPTION VALUE="Salles_combat">Salles de combat</OPTION> 
-	<OPTION VALUE="Skatepark">Skateparks</OPTION>
-	<OPTION VALUE="Bowlings">Bowlings</OPTION> 
-	<OPTION VALUE="Salles_remise_forme">Salles de remise en forme</OPTION>
-	<OPTION VALUE="Gymnase">Gymnases</OPTION> 
-	<OPTION VALUE="Port">Port</OPTION>
-	<OPTION VALUE="Randonnee">Randonnées</OPTION> 
-	<OPTION VALUE="Cinema">Cinémas</OPTION>
-	<OPTION VALUE="Boulodrome">Boulodromes</OPTION> 
-	<OPTION VALUE="Musee">Musées</OPTION>
-	<OPTION VALUE="Conservatoire">Conservatoires</OPTION> 
-	<OPTION VALUE="Theatre">Theâtres</OPTION> 
-	<OPTION VALUE="Tennis">Terrains de tennis</OPTION>
-	<OPTION VALUE="Cyclisme">Cyclisme</OPTION> 
-	<OPTION VALUE="Domaine_skiable">Domaines skiables</OPTION>
-	<OPTION VALUE="Centre_equestre">Centres équestres</OPTION>
-	<OPTION VALUE="Salles_combat">Salles de combat</OPTION> 
-	<OPTION VALUE="Athletisme">Athlétimse</OPTION>
-	<OPTION VALUE="Terrain_golf">Terrains de golf</OPTION> 
-	<OPTION VALUE="Parcours_sportifs">Parcours sportif</OPTION>
-	</SELECT>
+<form action="recuperation.php" method="get" >
+<SELECT name="loisirs" >
+<OPTION VALUE="">LOISIRS </OPTION>
+<OPTION VALUE="Piscine">Piscine</OPTION>
+<OPTION VALUE="Plateaux_sportifs">Plateaux Sportifs</OPTION> 
+<OPTION VALUE="Terrains_jeux">Terrains de jeux</OPTION> 
+<OPTION VALUE="Salles_combat">Salles de combat</OPTION> 
+<OPTION VALUE="Skatepark">Skateparks</OPTION>
+<OPTION VALUE="Bowling">Bowlings</OPTION> 
+<OPTION VALUE="Salles_remise_forme">Salles de remise en forme</OPTION>
+<OPTION VALUE="Gymnase">Gymnases</OPTION> 
+<OPTION VALUE="Port">Port</OPTION>
+<OPTION VALUE="Randonnee">Randonnées</OPTION> 
+<OPTION VALUE="Cinema">Cinémas</OPTION>
+<OPTION VALUE="Boulodrome">Boulodromes</OPTION> 
+<OPTION VALUE="Musee">Musées</OPTION>
+<OPTION VALUE="Conservatoire">Conservatoires</OPTION> 
+<OPTION VALUE="Theatre">Theâtres</OPTION> 
+<OPTION VALUE="Tennis">Terrains de tennis</OPTION>
+<OPTION VALUE="Cyclisme">Cyclisme</OPTION> 
+<OPTION VALUE="Domaine_skiable">Domaines skiables</OPTION>
+<OPTION VALUE="Centre_equestre">Centres équestres</OPTION>
+<OPTION VALUE="Salles_combat">Salles de combat</OPTION> 
+<OPTION VALUE="Athletisme">Athlétimse</OPTION>
+<OPTION VALUE="Terrain_golf">Terrains de golf</OPTION> 
+<OPTION VALUE="Parcours_sportifs">Parcours sportif</OPTION>
+</SELECT>
 
-	<SELECT name="commerces" >
-	<OPTION VALUE="">COMMERCES </OPTION>
-	<OPTION VALUE="Hypermarche">Hypermarché</OPTION>
-	<OPTION VALUE="Librairie">Librairie</OPTION>
-	<OPTION VALUE="Magasin_vetements">Magasin de vêtements</OPTION>
-	<OPTION VALUE="Magasins_foyer">Magasin du foyer</OPTION>
-	<OPTION VALUE="Magasin_chaussures">Magasin de chaussures</OPTION>
-	<OPTION VALUE="Magasin_electromenager">Magasin d'electromenager</OPTION>
-	<OPTION VALUE="Magasin_meubles">Magasin de meubles</OPTION>
-	<OPTION VALUE="Magasin_sport">Magasin de sport</OPTION>
-	<OPTION VALUE="Magasin_murs">Magasins de mur</OPTION>
-	<OPTION VALUE="Droguerie">Droguerie</OPTION>
-	<OPTION VALUE="Parfurmerie">Parfumerie</OPTION>
-	<OPTION VALUE="Supermarche">Supermarché</OPTION>
-	<OPTION VALUE="Bijouterie">Bijouterie</OPTION>
-	<OPTION VALUE="Fleuriste">Fleuriste</OPTION>
-	<OPTION VALUE="Magasin_optique">Magasin d'optique</OPTION>
-	<OPTION VALUE="Magasin_medical">Magasin du médical</OPTION>
-	<OPTION VALUE="Station_service">Station service</OPTION>
-	<OPTION VALUE="Bricolage">Bricolage</OPTION>
-	<OPTION VALUE="Superette">Supérette</OPTION>
-	<OPTION VALUE="Epicerie">Épicerie</OPTION>
-	<OPTION VALUE="Boulangerie">Boulangerie</OPTION>
-	<OPTION VALUE="Boucherie">Boucherie</OPTION>
-	<OPTION VALUE="Produits_surgeles">MAgasin de surgelés</OPTION>
-	<OPTION VALUE="Poissonnerie">Poissonnerie</OPTION>
-	</SELECT>	
 
-	<SELECT name="etablissementscolaire" >
-	<OPTION VALUE="">ETABLISSEMENTS SCOLAIRES </OPTION>
-	<OPTION VALUE="Ecole_maternelle">Ecole maternelle</OPTION>
-	<OPTION VALUE="Ecole_maternelle_cantine">Ecole maternelle (cantine)</OPTION>
-	<OPTION VALUE="Lycee_professionnel">Lycée professionnel</OPTION>
-	<OPTION VALUE="Lycee_professionnel_cantine">Lycée professionnel (cantine)</OPTION>
-	<OPTION VALUE="Lycee_professionnel_internat">Lycée professionnel (internat)</OPTION>
-	<OPTION VALUE="Lycee_technique">Lycée technologique</OPTION>
-	<OPTION VALUE="Lycee_technique_cantine">Lycée technologique (cantine)</OPTION>
-	<OPTION VALUE="Lycee_technique_internat">Lycée technologique (internat)</OPTION>
-	<OPTION VALUE="Prepa">Prépa</OPTION>
-	<OPTION VALUE="Formation_sante">Formation santé</OPTION>
-	<OPTION VALUE="Formation_commerce">Formation commerce</OPTION>
-	<OPTION VALUE="Formation_non_universitaire">Formation non universitaire</OPTION>
-	<OPTION VALUE="Formation_recherche">Formation recherche</OPTION>
-	<OPTION VALUE="Universite">Université</OPTION>
-	<OPTION VALUE="Ecole_ingenieur">École ingenieur</OPTION>
-	<OPTION VALUE="Enseignement_superieur_privee">Enseignement superieur privée</OPTION>
-	<OPTION VALUE="Enseignement_superieur_agricole">Enseignement superieur agricole</OPTION>
-	<OPTION VALUE="Enseignement_superieur_agricole_internat">Enseignement superieur agricole (internat)</OPTION>
-	<OPTION VALUE="Autres_enseignements_superieurs">Autres enseignements superieurs</OPTION>
-	<OPTION VALUE="Formation_sport">Formation sport</OPTION>
-	<OPTION VALUE="Residence_universitaire">Résidence universitaire</OPTION>
-	<OPTION VALUE="Ecole_elementaire_cantine">Ecole élémentaire (cantine)</OPTION>
-	<OPTION VALUE="College">Collège</OPTION>
-	<OPTION VALUE="College_cantine">Collège (cantine)</OPTION>
-	<OPTION VALUE="College_internat">Collège (internat)</OPTION>
-	<OPTION VALUE="Lycee_general_technologie">Lycée général technologique</OPTION>
-	<OPTION VALUE="Lycee_general_technologie_cantine">Lycée général technologique (cantine)</OPTION>
-	<OPTION VALUE="Lycee_general_technologie_internat">Lycée général technologique (internat) </OPTION>
-	</SELECT>
+
+<SELECT name="commerces" >
+<OPTION VALUE="">COMMERCES </OPTION>
+<OPTION VALUE="Hypermarche">Hypermarché</OPTION>
+<OPTION VALUE="Librairie">Librairie</OPTION>
+<OPTION VALUE="Magasin_vetements">Magasin de vêtements</OPTION>
+<OPTION VALUE="Magasin_foyer">Magasin du foyer</OPTION>
+<OPTION VALUE="Magasin_chaussures">Magasin de chaussures</OPTION>
+<OPTION VALUE="Magasin_electromenager">Magasin d'electromenager</OPTION>
+<OPTION VALUE="Magasin_meubles">Magasin de meubles</OPTION>
+<OPTION VALUE="Magasin_sport">Magasin de sport</OPTION>
+<OPTION VALUE="Magasin_murs">Magasins de mur</OPTION>
+<OPTION VALUE="Droguerie">Droguerie</OPTION>
+<OPTION VALUE="Parfumerie">Parfumerie</OPTION>
+<OPTION VALUE="Supermarche">Supermarché</OPTION>
+<OPTION VALUE="Bijouterie">Bijouterie</OPTION>
+<OPTION VALUE="Fleuriste">Fleuriste</OPTION>
+<OPTION VALUE="Magasin_optique">Magasin d'optique</OPTION>
+<OPTION VALUE="Magasin_medical">Magasin du médical</OPTION>
+<OPTION VALUE="Station_service">Station service</OPTION>
+<OPTION VALUE="Bricolage">Bricolage</OPTION>
+<OPTION VALUE="Superette">Supérette</OPTION>
+<OPTION VALUE="Epicerie">Épicerie</OPTION>
+<OPTION VALUE="Boulangerie">Boulangerie</OPTION>
+<OPTION VALUE="Boucherie">Boucherie</OPTION>
+<OPTION VALUE="Produits_surgeles">MAgasin de surgelés</OPTION>
+<OPTION VALUE="Poissonnerie">Poissonnerie</OPTION>
+</SELECT>
+
+
+<SELECT name="etablissementscolaire" >
+<OPTION VALUE="">ETABLISSEMENTS SCOLAIRES </OPTION>
+<OPTION VALUE="Ecole_maternelle">Ecole maternelle</OPTION>
+<OPTION VALUE="Ecole_maternelle_cantine">Ecole maternelle (cantine)</OPTION>
+<OPTION VALUE="Lycee_professionnel">Lycée professionnel</OPTION>
+<OPTION VALUE="Lycee_professionnel_cantine">Lycée professionnel (cantine)</OPTION>
+<OPTION VALUE="lycee_professionnel_internat">Lycée professionnel (internat)</OPTION>
+<OPTION VALUE="Lycee_technique">Lycée technologique</OPTION>
+<OPTION VALUE="Lycee_technique_cantine">Lycée technologique (cantine)</OPTION>
+<OPTION VALUE="Lycee_technique_internat">Lycée technologique (internat)</OPTION>
+<OPTION VALUE="Prepa">Prépa</OPTION>
+<OPTION VALUE="Formation_sante">Formation santé</OPTION>
+<OPTION VALUE="Formation_commerce">Formation commerce</OPTION>
+<OPTION VALUE="Formation_non_universitaire">Formation non universitaire</OPTION>
+<OPTION VALUE="Formation_recherche">Formation recherche</OPTION>
+<OPTION VALUE="Universite">Université</OPTION>
+<OPTION VALUE="Ecole_ingenieur">École ingenieur</OPTION>
+<OPTION VALUE="Enseignement_superieur_privee">Enseignement superieur privé</OPTION>
+<OPTION VALUE="Enseignement_superieur_agricole">Enseignement superieur agricole</OPTION>
+<OPTION VALUE="Enseignement_superieur_agricole_internat">Enseignement superieur agricole (internat)</OPTION>
+<OPTION VALUE="Autres_enseignements_superieurs">Autres enseignements superieurs</OPTION>
+<OPTION VALUE="Formation_sport">Formation sport</OPTION>
+<OPTION VALUE="Residence_universitaire">Résidence universitaire</OPTION>
+<OPTION VALUE="Ecole_elementaire_cantine">Ecole élémentaire (cantine)</OPTION>
+<OPTION VALUE="College">Collège</OPTION>
+<OPTION VALUE="College_cantine">Collège (cantine)</OPTION>
+<OPTION VALUE="College_internat">Collège (internat)</OPTION>
+<OPTION VALUE="Lycee_general_technologie">Lycée général technologique</OPTION>
+<OPTION VALUE="Lycee_general_technologie_cantine">Lycée général technologique (cantine)</OPTION>
+<OPTION VALUE="Lycee_general_technologie_internat">Lycée général technologique (internat) </OPTION>
+</SELECT>
 
 	<p style="padding-left: 250px"> <input type="submit" value="RECHERCHER"> </p> 
 
