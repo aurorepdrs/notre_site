66b4093e97c30c9029e251fd14a57bcbfe981421
KonateNindayou <60350204+KonateNindayou@users.noreply.github.com>
2020-04-10 15:54:59 +0200

    Add files via upload
    modif

--- PageSite/PagePhp/recuperation.php
+++ PageSite/PagePhp/recuperation.php
@@ -1,277 +1,241 @@
 <!DOCTYPE.html>
-<?php
-session_start();
-?>
+	<?php
+		session_start();
+	?>
 <html>
-<head>
+	<head>
 		
 		 <link rel="stylesheet" href="../accueil.css" type="text/css" />
+	
 	<?php
 	
 	$sql='';
 		
-	$sql1='SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, SE_TROUVER.Nombre_Loisirs AS L, POSSEDER.Nombre_Ecole AS E, 
-                FROM SOUS_QUARTIER, LOISIRS,Etablissement_Scolaire,POSSEDER,SE_TROUVER, QUARTIERS
-                WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
-                AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
-                AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
-                AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
-                and Etablissement_Scolaire.type_Ecole like ?
-                and LOISIRS.Type_loisirs like ?';
+	$sql1='SELECT distinct SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
+				FROM SOUS_QUARTIER, LOISIRS,Etablissement_Scolaire,POSSEDER,SE_TROUVER,QUARTIERS
+				WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
+				AND SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
+				AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
+				AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
+				and Etablissement_Scolaire.type_Ecole like ?
+				and POSSEDER.Nombre_Ecole>0
+				and LOISIRS.Type_loisirs like ?
+				and SE_TROUVER.Nombre_Loisirs>0';
                 
    		
-    $sql2='SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, SE_TROUVER.Nombre_Loisirs AS L,  AVOIR_COM.Nombre_C AS AN
-                FROM SOUS_QUARTIER, LOISIRS,COMMERCES,Etablissement_Scolaire,POSSEDER,SE_TROUVER,AVOIR_COM, QUARTIERS
-                WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
-                AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
-                AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
-                AND AVOIR_COM.IdCommerce=COMMERCES.IdCommerce
-                AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
-                AND SOUS_QUARTIER.CodeSQ=AVOIR_COM.CodeSQ
-                and COMMERCES.Type_Commerce like ? 
-                and LOISIRS.Type_loisirs like ?';
+   		
+    $sql2='SELECT distinct SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
+				FROM SOUS_QUARTIER, LOISIRS,COMMERCES,SE_TROUVER,AVOIR_COM, QUARTIERS
+				WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
+				AND AVOIR_COM.IdCommerce=COMMERCES.IdCommerce
+				AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
+				AND SOUS_QUARTIER.CodeSQ=AVOIR_COM.CodeSQ
+				and COMMERCES.Type_Commerce like ?
+				and AVOIR_COM.Nombre_C >0
+				and LOISIRS.Type_loisirs like ?
+				and SE_TROUVER.Nombre_Loisirs>0';
                
                
-    $sql3='SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, POSSEDER.Nombre_Ecole AS E, AVOIR_COM.Nombre_C AS AN
-                FROM SOUS_QUARTIER, LOISIRS,COMMERCES,Etablissement_Scolaire,POSSEDER,SE_TROUVER,AVOIR_COM, QUARTIERS
-                WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
-                AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
-                AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
-                AND AVOIR_COM.IdCommerce=COMMERCES.IdCommerce
-                AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
-                AND SOUS_QUARTIER.CodeSQ=AVOIR_COM.CodeSQ
-                and COMMERCES.Type_Commerce like ? and Etablissement_Scolaire.type_Ecole like ?';
+               
+    $sql3='SELECT distinct SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
+				FROM SOUS_QUARTIER, COMMERCES,Etablissement_Scolaire,POSSEDER,AVOIR_COM, QUARTIERS
+				WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
+				AND SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
+				AND AVOIR_COM.IdCommerce=COMMERCES.IdCommerce
+				AND SOUS_QUARTIER.CodeSQ=AVOIR_COM.CodeSQ
+				and COMMERCES.Type_Commerce like ?
+				and AVOIR_COM.Nombre_C >0
+				and Etablissement_Scolaire.type_Ecole like ?
+				and POSSEDER.Nombre_Ecole>0';
                
 
 
-	$sql4 = 'SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, SE_TROUVER.Nombre_Loisirs AS L, POSSEDER.Nombre_Ecole AS E, AVOIR_COM.Nombre_C AS AN
-                FROM SOUS_QUARTIER, LOISIRS,COMMERCES,Etablissement_Scolaire,POSSEDER,SE_TROUVER,AVOIR_COM, QUARTIERS
-                WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
-                AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
-                AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
-                AND AVOIR_COM.IdCommerce=COMMERCES.IdCommerce
-                AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
-                AND SOUS_QUARTIER.CodeSQ=AVOIR_COM.CodeSQ
-                and COMMERCES.Type_Commerce like ? and Etablissement_Scolaire.type_Ecole like ?
-                and LOISIRS.Type_loisirs like ?';
+	$sql4 = 'SELECT distinct SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
+				FROM SOUS_QUARTIER, LOISIRS,COMMERCES,Etablissement_Scolaire,POSSEDER,SE_TROUVER,AVOIR_COM, QUARTIERS
+				WHERE SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
+				AND SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
+				AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
+				AND AVOIR_COM.IdCommerce=COMMERCES.IdCommerce
+				AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
+				AND SOUS_QUARTIER.CodeSQ=AVOIR_COM.CodeSQ
+				and COMMERCES.Type_Commerce like ?
+				and AVOIR_COM.Nombre_C >0
+				and Etablissement_Scolaire.type_Ecole like ?
+				and POSSEDER.Nombre_Ecole>0
+				and LOISIRS.Type_loisirs like ?
+				and SE_TROUVER.Nombre_Loisirs>0';
                
                 
 	
-	$sql5= 'SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, AVOIR_COM.Nombre_C AS AN
-			FROM SOUS_QUARTIER, QUARTIERS, COMMERCES, AVOIR_COM
-			WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
-
-			AND COMMERCES.IdCommerce=AVOIR_COM.IdCommerce
-			AND AVOIR_COM.CodeSQ=SOUS_QUARTIER.CodeSQ
-			AND COMMERCES.Type_Commerce=?';
+	$sql5= 'SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
+				FROM SOUS_QUARTIER, QUARTIERS, COMMERCES, AVOIR_COM
+				WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND COMMERCES.IdCommerce=AVOIR_COM.IdCommerce
+				AND AVOIR_COM.CodeSQ=SOUS_QUARTIER.CodeSQ
+				AND COMMERCES.Type_Commerce like? 
+				AND AVOIR_COM.Nombre_C >0';
 			
 	
 	
-	$sql6= 'SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, SE_TROUVER.Nombre_Loisirs AS L
-		FROM SOUS_QUARTIER, QUARTIERS, SE_TROUVER, LOISIRS
-			WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
-			AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
-			AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
-			AND LOISIRS.Type_loisirs=?';
+	$sql6= 'SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
+				FROM SOUS_QUARTIER, QUARTIERS, SE_TROUVER, LOISIRS
+				WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND SOUS_QUARTIER.CodeSQ=SE_TROUVER.CodeSQ
+				AND SE_TROUVER.Id_loisirs=LOISIRS.Id_loisirs
+				AND LOISIRS.Type_loisirs like?
+				and SE_TROUVER.Nombre_Loisirs>0';
 			
 		
-		$sql7='SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, posseder.Nombre_Ecole AS E
+		
+	$sql7='SELECT DISTINCT SOUS_QUARTIER.Nom AS SQN, QUARTIERS.Nom AS Q
 				FROM SOUS_QUARTIER, QUARTIERS, POSSEDER, Etablissement_Scolaire
-					WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
-					AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
+				WHERE SOUS_QUARTIER.CodeQ=QUARTIERS.CodeQ
+				AND Etablissement_Scolaire.idEcole=POSSEDER.IdEcole
 				AND SOUS_QUARTIER.CodeSQ=POSSEDER.CodeSQ
-				AND Etablissement_Scolaire.type_Ecole=?';
+				AND Etablissement_Scolaire.type_Ecole like ?
+				and POSSEDER.Nombre_Ecole>0';
 				
+  
    		
 			if ($_GET['etablissementscolaire']!='' AND  $_GET['loisirs']!='' AND $_GET['commerces']=='' ){
-			$sql=$sql.$sql1;
-			
+				$sql=$sql.$sql1;
                 $bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
+				$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$req=$bdd->prepare($sql);
+				$req->execute(array($_GET['etablissementscolaire'],$_GET['loisirs']));
+				echo '<h1>ETABLISSEMENTS SCOLAIRES ET LOISIRS</h1>';
+				while ($donnees = $req->fetch()){
+					 echo $donnees['Q'];
+					 ECHO '       :';
+				 	 echo $donnees['SQN'];
+				 	 ECHO '<br/>';
+				}
+				$req->closeCursor();
 			
-			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			
-			$req=$bdd->prepare($sql);
-			
-			$req->execute(array($_GET['etablissementscolaire'],$_GET['loisirs']));
-			
-			echo '<h1>Nombre Etablissements Scolaires et Types de loisirs par sous-quartier</h1>';
-			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			 echo $donnees['E'].' ';
-			
-			 echo $_GET['etablissementscolaire'];
-			 echo','.$donnees['L'].' ';
-			 echo $_GET['loisirs'];
-			
-			 echo '  dans le quartier  ';
-			 echo $donnees['SQN'];
-			 ECHO '<br/>';
-			 
-			}
-		
-		
-			$req->closeCursor();
-			
-               
-                }
+            }
+            
+            
+            
    		if ($_GET['commerces']!='' AND $_GET['loisirs']!='' AND $_GET['etablissementscolaire']=='' ){
-   		
-                  $bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
-			
+   			$sql=$sql.$sql2;
+            $bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
 			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$req=$bdd->prepare($sql);
 			$req->execute(array($_GET['commerces'],$_GET['loisirs']));
-			echo '<h1>Nombre de commerces et Types de Loisirs par sous-quartier</h1>';
+			echo '<h1>COMMERCES ET LOISIRS</h1>';
 			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			
-			 echo','.$donnees['L'].' ';
-			 echo $_GET['loisirs'];
-			 echo','.$donnees['AN'].' ';
-			 echo $_GET['commerces'];
-			 echo '  dans le quartier  ';
-			 echo $donnees['SQN'];
-			 ECHO '<br/>';
-			 
+			 echo $donnees['Q'];
+					 ECHO '      :';
+				 	echo $donnees['SQN'];
+				 	ECHO '<br/>';
 			}
-		
-		
 			$req->closeCursor();
-			
                 }
+                
+              
+                
          if ($_GET['commerces']!='' AND $_GET['etablissementscolaire']!='' AND $_GET['loisirs']==''){ 
-           
+           	$sql=$sql.$sql3;
                $bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
 			
 			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$req=$bdd->prepare($sql);
 			$req->execute(array($_GET['commerces'],$_GET['etablissementscolaire']));
-			echo '<h1>Nombre de Commerces et Etablissements Scolaires par sous-quartier</h1>';
+			echo '<h1>COMMERCES ET ETABLISSEMENTS SCOLAIRES</h1>';
 			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			 echo $donnees['E'].' ';
-			
-			 echo $_GET['etablissementscolaire'];
-			
-			 echo','.$donnees['AN'].' ';
-			 echo $_GET['commerces'];
-			 echo '  dans le quartier  ';
+			echo $donnees['Q'];
+			echo'      :';
 			 echo $donnees['SQN'];
 			 ECHO '<br/>';
-			 
 			}
-		
-		
 			$req->closeCursor();
-			
                 }
+			
+			
 				
 			if ($_GET['commerces']!='' AND $_GET['etablissementscolaire']!='' AND $_GET['loisirs']!='' ){
 				 $sql=$sql.$sql4;
-               
-                $bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
-			
-			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$req=$bdd->prepare($sql);
-			$req->execute(array($_GET['commerces'],$_GET['etablissementscolaire'],$_GET['loisirs']));
-			echo '<h1>Nombre Etablissements scolaires, Types de Loisirs et commerces par sous-quartiers</h1>';
-			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			 echo $donnees['E'].' ';
-			
-			 echo $_GET['etablissementscolaire'];
-			 echo','.$donnees['L'].' ';
-			 echo $_GET['loisirs'];
-			 echo','.$donnees['AN'].' ';
-			 echo $_GET['commerces'];
-			 echo '  dans le quartier  ';
-			 echo $donnees['SQN'];
-			 ECHO '<br/>';
-			 
+                 $bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
+			 	 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				 $req=$bdd->prepare($sql);
+				 $req->execute(array($_GET['commerces'],$_GET['etablissementscolaire'],$_GET['loisirs']));
+				 echo '<h1>ETABLISSEMENTS SCOLAIRES, COMMERCES ET LOISIRS</h1>';
+				 while ($donnees = $req->fetch()){
+					echo $donnees['Q'];
+					echo'       :';
+			 		echo $donnees['SQN'];
+			 		ECHO '<br/>';
 			}
-		
-		
 			$req->closeCursor();
-			
-			}
-                
-               if ($_GET['commerces']!='' AND $_GET['loisirs']=='' AND $_GET['etablissementscolaire']=='' ){
-                $sql=$sql.$sql5;
-				$bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
-			
-			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$req=$bdd->prepare($sql);
-			$req->execute(array($_GET['commerces']));
-			echo '<h1>Nombre de commerces par sous-quartiers</h1>';
-			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			 echo $donnees['AN'].' ';
-			 echo $_GET['commerces'];
-			 echo '  dans le quartier  ';
-			 echo $donnees['SQN'];
-			 ECHO '<br/>';
-			
-    			
-			}
-		
-			$req->closeCursor();
-			
 				}
+                
+        
+        
+               if ($_GET['commerces']!='' AND $_GET['loisirs']=='' AND $_GET['etablissementscolaire']=='' ){
+               	 	$sql=$sql.$sql5;
+					$bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
+					$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					$req=$bdd->prepare($sql);
+					$req->execute(array($_GET['commerces']));
+					echo '<h1>COMMERCES</h1>';
+					while ($donnees = $req->fetch()){
+					echo $donnees['Q'];
+					echo'       :';
+				    echo $donnees['SQN'];
+			 		ECHO '<br/>';	
+			}
+			$req->closeCursor();
+				}
+				
+				
 				
 				if ($_GET['loisirs']!='' AND $_GET['etablissementscolaire']=='' AND $_GET['commerces']==''  ){
 					$sql=$sql.$sql6;
-					
 					$bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
-			echo '<h1>Nombre de Centres de loisirs par sous-quartiers</h1>';
-			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$req=$bdd->prepare($sql);
-			$req->execute(array($_GET['loisirs']));
-		
-			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			 echo $donnees['L'].' ';
-			 echo $_GET['loisirs'];
-			 echo '  dans le quartier  ';
-			 echo $donnees['SQN'];
-			 ECHO '<br/>';
-			 
-    			
+					echo '<h1>LOISIRS</h1>';
+					$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					$req=$bdd->prepare($sql);
+					$req->execute(array($_GET['loisirs']));
+					while ($donnees = $req->fetch()){
+						echo $donnees['Q'];
+						echo'       :';
+						echo $donnees['SQN'];
+						ECHO '<br/>';
 			}
-		
 			$req->closeCursor();
 			}
-					
-					if ( $_GET['etablissementscolaire']!='' AND $_GET['loisirs']=='' AND $_GET['commerces']=='' ){
-					$sql=$sql.$sql7;
-									$bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
 			
-			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$req=$bdd->prepare($sql);
-			$req->execute(array($_GET['etablissementscolaire']));
-			echo '<h1>Nombre Etablissements scolaires par sous-quartiers</h1>';
-			while ($donnees = $req->fetch()){
-			 echo 'il ya  ';
-			 echo $donnees['E'].' ';
-			 echo $_GET['etablissementscolaire'];
-			 echo '  dans le quartier  ';
-			 echo $donnees['SQN'];
-			 ECHO '<br/>';
-			 
+				
+					
+				if ( $_GET['etablissementscolaire']!='' AND $_GET['loisirs']=='' AND $_GET['commerces']=='' ){
+					$sql=$sql.$sql7;
+					$bdd = new PDO('mysql:host=localhost;dbname=COMMERCES', 'root', 'root');
+			
+					$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					$req=$bdd->prepare($sql);
+					$req->execute(array($_GET['etablissementscolaire']));
+					echo '<h1>ETABLISSEMENTS SCOLAIRES</h1>';
+					while ($donnees = $req->fetch()){
+						 echo $donnees['Q'];
+					 	 ECHO '       :';
+				 		 echo $donnees['SQN'];
+				 	 	 ECHO '<br/>';
 			}
-		
 			$req->closeCursor();
 			}               
 			
 			
+			
 			$ok=TRUE;
-		if ($_GET['commerces']=='' AND $_GET['loisirs']=='' AND $_GET['etablissementscolaire']==''){
-			$ok=FALSE;
-			echo 'VEUILLEZ SELECTIONNER AU MOINS UN CHAMP...';
-			echo"<meta http-equiv='refresh' content='1; URL=PageAccueil.php'>";	
+			if ($_GET['commerces']=='' AND $_GET['loisirs']=='' AND $_GET['etablissementscolaire']==''){
+				$ok=FALSE;
+				echo 'VEUILLEZ SELECTIONNER AU MOINS UN CHAMP...';
+				echo"<meta http-equiv='refresh' content='1; URL=PageAccueil.php'>";	
 }
-		else{
-			echo'';
-		}
 			
 		?>
 <title>recuperation </title>
